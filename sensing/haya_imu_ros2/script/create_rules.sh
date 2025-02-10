1a4e86686ec0137eeee9e86a360e477b3112588b
motii <108280115+motii8128@users.noreply.github.com>
2025-02-10 22:06:43 +0900

    Add files via upload

--- sensing/haya_imu_ros2/script/create_rules.sh
+++ sensing/haya_imu_ros2/script/create_rules.sh
@@ -2,7 +2,7 @@
 echo "remap the device serial port(ttyACM*) to haya_imu"
 echo "haya_imu usb connection as /dev/haya_imu, check it using command: ls -l /dev/ttyACM*"
 echo "start copy haya_imu.rules to /etc/udev/rules.d/"ros
-sudo cp ~/ros2_ws/src/haya_imu_ros2/script/haya_imu.rules /etc/udev/rules.d
+sudo cp ~/ros2_ws/src/Accurate/sensing/haya_imu_ros2/script/haya_imu.rules /etc/udev/rules.d
 echo ""
 echo "restarting udev..."
 echo ""
