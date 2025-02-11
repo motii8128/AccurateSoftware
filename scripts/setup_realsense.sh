8283a04e75388d729318a041a7cd2d2fbc6ff0d1
motii8128 <default270robo@gmail.com>
2025-02-11 14:43:41 +0900

    Fix scripts

--- scripts/setup_realsense.sh
+++ scripts/setup_realsense.sh
@@ -20,4 +20,8 @@ echo "----- Install RealSense Library -----"
 
 sudo apt-get install librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg
 
+echo "----- Install RealSense ROS2 Driver -----"
+
+sudo apt install ros-humble-realsense2-*
+
 echo "----- Finished setup RealSense -----"
