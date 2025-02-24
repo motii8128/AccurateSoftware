f8655615d5c397b0171bdae282f92a28f83417f0
motii8128 <default270robo@gmail.com>
2025-02-24 18:21:57 +0900

    add setup script

---
+++ scripts/setup_accurate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "----- Start Accurate Software require development setup!! -----"
+
+chmod +x ./setup_ydlidar_sdk.sh
+./setup_ydlidar_sdk.sh
+
+chmod +x ./setup_realsense.sh
+./setup_realsense.sh
+
+chmod +x ./setup_obj_detector.sh
+./setup_obj_detector.sh
---
+++ scripts/setup_obj_detector.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "----- Start ZeroShot Object Detector Setup -----"
+
+pip install transformers torch torchvision opencv-python cv-bridge
+
+echo "----- Finished ZeroShot Object Detector Setup !! -----"
