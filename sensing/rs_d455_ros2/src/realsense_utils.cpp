cd62dd3739416cde41b74cdc16a3eed6d6ce34d3
motii8128 <default270robo@gmail.com>
2025-03-01 11:21:57 +0900

    update

--- sensing/rs_d455_ros2/src/realsense_utils.cpp
+++ sensing/rs_d455_ros2/src/realsense_utils.cpp
@@ -14,11 +14,12 @@ namespace rs_d455_ros2
                 dev.hardware_reset();                
                 rs2::config cfg;
                 cfg.disable_all_streams();
-                cfg.enable_stream(RS2_STREAM_COLOR, width_, height_, RS2_FORMAT_RGB8, fps_);
+                cfg.enable_stream(RS2_STREAM_COLOR, width_, height_, RS2_FORMAT_BGR8, fps_);
+                cfg.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_BGR8, 30);
                 cfg.enable_stream(RS2_STREAM_DEPTH, width_, height_, RS2_FORMAT_Z16, fps_);
-                cfg.enable_device(std::string(dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER)));
                 cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F, 250);
                 cfg.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F, 200);
+                cfg.enable_device(std::string(dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER)));
                 pipe_.start(cfg);
             }
             catch (const rs2::error& e) {
