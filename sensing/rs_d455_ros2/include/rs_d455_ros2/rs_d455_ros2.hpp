48bd80f8073c99715eec149daf0ce9d1a50845d9
motii8128 <default270robo@gmail.com>
2025-03-01 14:13:09 +0900

    fix

--- sensing/rs_d455_ros2/include/rs_d455_ros2/rs_d455_ros2.hpp
+++ sensing/rs_d455_ros2/include/rs_d455_ros2/rs_d455_ros2.hpp
@@ -3,7 +3,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
-#include <cv_bridge/cv_bridge.hpp>
+#include <cv_bridge/cv_bridge.h>
 
 #include <chrono>
 #include "realsense_utils.hpp"
